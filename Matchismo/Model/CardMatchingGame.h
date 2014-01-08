59d60ba9d8d8798d7e795046ffd654a282d52bdf
Nick Freeman <nickafreeman@berkeley.edu>
2014-01-08 01:45:00 -0800

    Matchismo matching algorithm completed for one card

---
+++ Matchismo/Model/CardMatchingGame.h
@@ -0,0 +1,23 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by Nick Freeman on 1/8/14.
+//  Copyright (c) 2014 Nick Freeman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Deck.h"
+
+@interface CardMatchingGame : NSObject
+
+// designated initializer
+- (instancetype)initWithCardCount:(NSUInteger)count
+                        usingDeck:(Deck *)deck;
+
+- (void)chooseCardAtIndex:(NSUInteger)index;
+- (Card *)cardAtIndex:(NSUInteger)index;
+
+@property (nonatomic, readonly) NSInteger score;
+
+@end
