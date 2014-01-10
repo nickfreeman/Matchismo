c62d4656f83c00fb3d78f9c89a0f8dd40b7eb9f8
Nick Freeman <nickafreeman@berkeley.edu>
2014-01-09 20:20:36 -0800

    added button to start new game

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -19,6 +19,12 @@
 
 @implementation CardGameViewController
 
+- (IBAction)startNewGame:(UIButton *)sender {
+    _game = [[CardMatchingGame alloc] initWithCardCount:[self.cardButtons count]
+                                              usingDeck:[self createDeck]];
+    [self updateUI];
+}
+
 - (CardMatchingGame *)game {
     if (!_game) _game = [[CardMatchingGame alloc] initWithCardCount:[self.cardButtons count]
                                                           usingDeck:[self createDeck]];
@@ -42,8 +48,8 @@
         [cardButton setTitle:[self titleForCard:card] forState:UIControlStateNormal];
         [cardButton setBackgroundImage:[self backgroundImageForCard:card] forState:UIControlStateNormal];
         cardButton.enabled = !card.isMatched;
+        self.scoreLabel.text = [NSString stringWithFormat:@"Score: %ld", self.game.score];
     }
-    self.scoreLabel.text = [NSString stringWithFormat:@"Score: %ld", self.game.score];
 }
 
 - (NSString *)titleForCard:(Card *)card {
